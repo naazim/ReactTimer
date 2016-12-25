f2a471f758ffed3c0ad351184f25bf4b4ac43065
naazim <naazim4u@gmail.com>
2016-12-25 17:28:37 +0100

    test cases updated for countdown form

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,30 +1,34 @@
 var React = require('react');
 
 var Clock = React.createClass({
-  getDefaultProps: function(){
+  getDefaultProps: function () {
     totalSeconds: 0
   },
-  PropTypes: {
+  propTypes: {
     totalSeconds: React.PropTypes.number
   },
-  formatSeconds: function(totalSeconds){
+  formatSeconds: function (totalSeconds) {
     var seconds = totalSeconds % 60;
     var minutes = Math.floor(totalSeconds / 60);
 
-    if(seconds < 10) {
+    if (seconds < 10) {
       seconds = '0' + seconds;
     }
-    if(minutes < 10){
+
+    if (minutes < 10) {
       minutes = '0' + minutes;
     }
 
     return minutes + ':' + seconds;
   },
-  render: function(){
+  render: function () {
     var {totalSeconds} = this.props;
+
     return (
       <div className="clock">
-        <span className="clock-text">{this.formatSeconds(totalSeconds)}</span>
+        <span className="clock-text">
+          {this.formatSeconds(totalSeconds)}
+        </span>
       </div>
     );
   }
